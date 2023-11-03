badc0e6aeb8ac8fb733b83ddd41a9e8d3dbb4304
Joshua <jaauwyan@uwaterloo.ca>
2023-11-03 19:25:49 -0400

    added new function to process colour sensor values to detect target colours and estimate line position relative to robot heading with general PID algorithm

---
+++ Core/Src/line_following.c
@@ -0,0 +1,22 @@
+static const double SENSOR_COUNT = 5;
+
+// PID Gain Constants
+static const double Kp = 1;
+static const double Kd = 0;
+static const double Ki = 0;
+
+double previousError = 0;
+
+double PID_calculator(double linePosition) {
+	double error = GOAL - linePosition;
+
+	double errorDerivative = error - previousError;
+	double errorIntegral = errorIntegral + error;
+
+	double steeringValue = Kp*error + Ki*errorIntegral + Kd*errorDerivative;
+
+	previousError = error;
+
+	return steeringValue;
+	//TODO: SCALE THE VALUES SO THAT (I.E. HOW MUCH THE THING SHOULD TURN)
+}
