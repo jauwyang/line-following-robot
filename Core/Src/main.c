6fc48d430ff27ed217947cd113979d4b32f8d977
Henchel Santillan <henchelsantillan@gmail.com>
2023-11-05 23:41:34 -0500

    Add colour sensor logic to branch

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,13 +18,13 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "state_machine/state_machine.h"
-#include "IR_sensor_version/IR_line_following.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "mg995/mg995.h"
 #include "l298n/l298n.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 /* USER CODE END Includes */
@@ -47,6 +47,7 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
 
@@ -60,14 +61,21 @@ static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+volatile uint32_t start_val = 0;
+volatile uint32_t end_val = 0;
+volatile uint32_t difference = 0;
+volatile bool was_start_captured = false;
+
 static uint32_t ir_values[IR_COUNT] = {0};
-char msg[128];
+char msg[128] = {0};
 
 static void read_ir_sensors(void) {
 	ir_values[0] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0);
@@ -75,6 +83,27 @@ static void read_ir_sensors(void) {
 	ir_values[2] = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_2);
 }
 
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
+		if (!was_start_captured) {
+			start_val = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+			was_start_captured = true;
+
+			__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+
+		} else {
+			end_val = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+			__HAL_TIM_SET_COUNTER(htim, 0);
+
+			difference = (end_val > start_val) ? end_val - start_val
+											   : (0xFFFF - start_val) + end_val;
+
+			was_start_captured = false;
+			__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+			__HAL_TIM_DISABLE_IT(&htim1, TIM_IT_CC1);
+		}
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -108,6 +137,7 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM1_Init();
   MX_USART2_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   // MG995 Servo Motor
@@ -117,6 +147,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
 
+  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
 
   // Initialize Left and Right Motors
   motor_t motor_left = {
@@ -130,10 +161,21 @@ int main(void)
           .in_pins = { GPIO_PIN_4, GPIO_PIN_5 },
           .pwm_channel = TIM_CHANNEL_2
   };
+
+  // 20% frequency scaling
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+
+  uint32_t red = 0;
+  uint32_t blue = 0;
+  uint32_t green = 0;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
+  // Wait for 10 seconds before beginning the main loop
   HAL_Delay(10000);
   while (1)
   {
@@ -143,14 +185,35 @@ int main(void)
 	  l298n_move_fwd_single(&motor_left, MAX_PWM);
 	  l298n_move_fwd_single(&motor_right, MAX_PWM);
 
+	  // Detect Red
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+	  HAL_Delay(5);
+	  __HAL_TIM_ENABLE_IT(&htim1, TIM_IT_CC1);
+	  HAL_Delay(5);
+	  red = difference;
+
+	  // Detect Blue
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+	  HAL_Delay(5);
+	  __HAL_TIM_ENABLE_IT(&htim1, TIM_IT_CC1);
+	  HAL_Delay(5);
+	  blue = difference;
+
+	  // Detect Green
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+	  HAL_Delay(5);
+	  __HAL_TIM_ENABLE_IT(&htim1, TIM_IT_CC1);
+	  HAL_Delay(5);
+	  green = difference;
+
 //	  read_ir_sensors();
 //	  IR_followLine(&motor_left, &motor_right, ir_values, IR_COUNT);
 
-
-	  // Uncomment for testing
-	  /*sprintf(msg, "%hu %hu %hu\r\n", ir_values[0], ir_values[1], ir_values[2]);
+	  sprintf(msg, "Red: %hu\r\n Blue: %hu\r\n Green: %hu\r\n\r\n", red, blue, green);
 	  HAL_UART_Transmit(&huart2, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
-	  HAL_Delay(2000);*/
   }
   /* USER CODE END 3 */
 }
@@ -340,6 +403,64 @@ static void MX_TIM2_Init(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 84-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -389,13 +510,16 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PC4 PC5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
+  /*Configure GPIO pins : PC4 PC5 PC6 PC7
+                           PC8 PC9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
