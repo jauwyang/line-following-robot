97b7db11e74ede5aa1dc7493cfb30d868e2a02a8
Joshua <jaauwyan@uwaterloo.ca>
2023-11-21 15:52:26 -0500

    changed start sequence of state machine

--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -11,14 +11,18 @@
 // create
 
 void start(enum RobotSequence *currentState, motor_t *motorLeft, motor_t *motorRight) {
-	uint16_t motorPWM = 1; // TODO: Change later (not max?)
-	tb6612fng_move_fwd(motorLeft, motorRight, motorPWM, motorPWM);
+//	uint16_t motorPWM = 1; // TODO: Change later (not max?)
+//	tb6612fng_move_fwd(motorLeft, motorRight, motorPWM, motorPWM);
+//
+//	// transition state condition (starting line detected)
+//	uint32_t requiredSensorColourMatches = 4;
+//	if (countMatchingSensorColourDetections(RED) >= requiredSensorColourMatches){
+//		*currentState = FOLLOW_LINE_TO_TARGET;
+//	}
 
-	// transition state condition (starting line detected)
-	uint32_t requiredSensorColourMatches = 4;
-	if (countMatchingSensorColourDetections(RED) >= requiredSensorColourMatches){ 
-		*currentState = FOLLOW_LINE_TO_TARGET;
-	}
+
+	// transition state condition (switch automatically, START only used as entry point)
+	*currentState = FOLLOW_LINE_TO_TARGET;
 }
 
 void followLineToTarget(enum RobotSequence *currentState, motor_t *motorLeft, motor_t *motorRight){
